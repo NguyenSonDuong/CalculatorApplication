b24269993218a31ee164dede736d724233a28d28
NguyenSonDuong <hunterdemon9x99@gmail.com>
2020-01-11 13:47:01 +0700

    cập nhật lại thuật toán xử lý biểu thức thêm 1 số ngoại lệ

--- app/src/main/java/com/example/calculatorapplication/XuLy.java
+++ app/src/main/java/com/example/calculatorapplication/XuLy.java
@@ -7,79 +7,91 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class XuLy {
-    public static GenQueue tachChuoi(String input) {
+
+    public static ArrayList getItemList(String input) {
         ArrayList<String> temb = new ArrayList<>();
         Pattern pa = Pattern.compile("-[0-9]{1,12}|[0-9]{1,12}|\\.|[+\\/()x$]");
         Matcher mat = pa.matcher(input);
-        int i = 0;
-        Stack<String> stack = new Stack<>();
-        GenQueue<String> queue = new GenQueue<>();
-
         while (mat.find()) {
             temb.add(mat.group(0));
         }
+        return temb;
+    }
+    public static GenQueue splitString(String input) throws Exception{
+        if(!isDone(input))
+            return null;
+        ArrayList<String> temb = getItemList(input);
+        int i = 0;
+        Stack<String> stack = new Stack<>();
+        GenQueue<String> queue = new GenQueue<>();
         int a = 0;
         String matString = "";
-        while (a < temb.size()) {
-            matString = temb.get(a);
-            try {
-                if (matString.equals(".")) {
-                    a++;
-                    matString = queue.dequeue3() + "." + temb.get(a);
-                }
-                Double b = Double.parseDouble(matString.trim());
-                queue.enqueue(matString);
-            } catch (Exception ex) {
-                if (stack.isEmpty()) {
-                    stack.push(matString);
-                } else {
-                    if (matString.equals("+") || matString.equals("$")) {
-                        if (stack.peek().equals("+") || stack.peek().equals("$")) {
-                            while (!stack.empty()) {
-                                queue.enqueue(stack.pop());
-                            }
-                            stack.push(matString);
-                        } else if (stack.peek().equals("x") || stack.peek().equals("/")) {
-                            while (!stack.empty()) {
-                                queue.enqueue(stack.pop());
-                            }
-                            stack.push(matString);
-                        } else if (stack.peek().equals("(")) {
-                            stack.push(matString);
-                        }
-                    } else if (matString.equals("x") || matString.equals("/")) {
-                        if (stack.peek().equals("+") || stack.peek().equals("$")) {
-                            stack.push(matString);
-                        } else if (stack.peek().equals("x") || stack.peek().equals("/")) {
-                            while (!stack.empty()) {
-                                queue.enqueue(stack.pop());
-                            }
-                            stack.push(matString);
-                        } else if (stack.peek().equals("(")) {
-                            stack.push(matString);
-                        }
-                    } else if (matString.equals("(")) {
-                        stack.push(matString);
-
-                    } else if (matString.equals(")")) {
-                        while (!stack.peek().equals("(")) {
-                            queue.enqueue(stack.pop());
-                        }
-                        stack.pop();
+        try{
+            while (a < temb.size()) {
+                matString = temb.get(a);
+                try {
+                    if (matString.equals(".")) {
+                        a++;
+                        matString = queue.dequeue3() + "." + temb.get(a);
                     }
+                    Double b = Double.parseDouble(matString.trim());
+                    queue.enqueue(matString);
+                } catch (Exception ex) {
+                    if (stack.isEmpty()) {
+                        stack.push(matString);
+                    } else {
+                        if (matString.equals("+") || matString.equals("$")) {
+                            if (stack.peek().equals("+") || stack.peek().equals("$")) {
+                                while (!stack.empty()) {
+                                    queue.enqueue(stack.pop());
+                                }
+                                stack.push(matString);
+                            } else if (stack.peek().equals("x") || stack.peek().equals("/")) {
+                                while (!stack.empty()) {
+                                    queue.enqueue(stack.pop());
+                                }
+                                stack.push(matString);
+                            } else if (stack.peek().equals("(")) {
+                                stack.push(matString);
+                            }
+                        } else if (matString.equals("x") || matString.equals("/")) {
+                            if (stack.peek().equals("+") || stack.peek().equals("$")) {
+                                stack.push(matString);
+                            } else if (stack.peek().equals("x") || stack.peek().equals("/")) {
+                                while (!stack.empty()) {
+                                    queue.enqueue(stack.pop());
+                                }
+                                stack.push(matString);
+                            } else if (stack.peek().equals("(")) {
+                                stack.push(matString);
+                            }
+                        } else if (matString.equals("(")) {
+                            stack.push(matString);
+
+                        } else if (matString.equals(")")) {
+                            while (!stack.peek().equals("(")) {
+                                queue.enqueue(stack.pop());
+                            }
+                            stack.pop();
+                        }
+                    }
+
                 }
 
+                a++;
             }
-
-            a++;
-        }
-        while (!stack.empty()) {
-            queue.enqueue(stack.pop());
+            while (!stack.empty()) {
+                queue.enqueue(stack.pop());
+            }
+        }catch (Exception ex){
+            throw new Exception("Lỗi nhập liệu");
         }
         return queue;
     }
-    public static double kq(String input) {
-        GenQueue<String> queue = tachChuoi(input);
+    public static double calculator(String input) throws Exception{
+        GenQueue<String> queue = splitString(input);
+        if(queue == null)
+            throw new Exception("Lỗi nhập liệu vui lòng kiểm tra đóng mở ngoặc hoặc dấu phẩy");
         Stack<Double> kq = new Stack<>();
         double g = 0;
         while (queue.hasItems()) {
@@ -89,26 +101,30 @@ public class XuLy {
                 double temb = Double.parseDouble(item);
                 kq.push(temb);
             } catch (Exception ex) {
-                if (item.equals("+")) {
-                    double num2 = kq.pop();
-                    double num1 = kq.pop();
-                    double kq1 = num1 + num2;
-                    kq.push(kq1);
-                } else if (item.equals("$")) {
-                    double num2 = kq.pop();
-                    double num1 = kq.pop();
-                    double kq1 = num1 - num2;
-                    kq.push(kq1);
-                } else if (item.equals("x")) {
-                    double num2 = kq.pop();
-                    double num1 = kq.pop();
-                    double kq1 = num1 * num2;
-                    kq.push(kq1);
-                } else if (item.equals("/")) {
-                    double num2 = kq.pop();
-                    double num1 = kq.pop();
-                    double kq1 = num1 / num2;
-                    kq.push(kq1);
+                try{
+                    if (item.equals("+")) {
+                        double num2 = kq.pop();
+                        double num1 = kq.pop();
+                        double kq1 = num1 + num2;
+                        kq.push(kq1);
+                    } else if (item.equals("$")) {
+                        double num2 = kq.pop();
+                        double num1 = kq.pop();
+                        double kq1 = num1 - num2;
+                        kq.push(kq1);
+                    } else if (item.equals("x")) {
+                        double num2 = kq.pop();
+                        double num1 = kq.pop();
+                        double kq1 = num1 * num2;
+                        kq.push(kq1);
+                    } else if (item.equals("/")) {
+                        double num2 = kq.pop();
+                        double num1 = kq.pop();
+                        double kq1 = num1 / num2;
+                        kq.push(kq1);
+                    }
+                }catch (Exception e){
+                    throw new Exception("Lỗi nhập liệu");
                 }
             }
 
@@ -116,7 +132,30 @@ public class XuLy {
 
         return kq.pop();
     }
+    public static boolean isDone(String input) {
+        ArrayList<String> temb = getItemList(input);
+        int demMoNgoac = 0;
+        int demDongNgoac = 0;
+        for (int i = 0; i < temb.size(); i++) {
+            if (temb.get(i).equals("("))
+                demMoNgoac++;
+            else if (temb.get(i).equals(")"))
+                demDongNgoac++;
+        }
+        if(demDongNgoac != demMoNgoac)
+            return false;
+        for (int i = 0; i < temb.size(); i++) {
+            if (temb.get(i).equals(".") && temb.get(i+1).equals("."))
+                return false;
+        }
+        for (int i = 0; i < temb.size(); i++) {
+            if (temb.get(i).equals("-") && temb.get(i+1).equals("-"))
+                return false;
+        }
+        return true;
+    }
 }
+
 class GenQueue<E> {
 
     public LinkedList<E> list = new LinkedList<E>();
@@ -125,9 +164,10 @@ class GenQueue<E> {
         list.addLast(item);
     }
 
-    public E dequeue3(){
+    public E dequeue3() {
         return list.pollLast();
     }
+
     public E dequeue() {
         return list.poll();
     }
